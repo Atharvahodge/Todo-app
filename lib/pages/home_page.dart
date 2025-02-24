0f9f8cd3f4288ae9dd39ce3c786e211fbe8d2de3
Atharvahodge <atharvahodge16@gmail.com>
2025-02-24 13:02:46 +0530

    feat(ui): add feature to delete task
     added a feature to delete the task from the app and improved some ui changes.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,14 +9,31 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _controller = TextEditingController();
   List toDOList = [
     ['Create todo', false],
     ['Read to type', false],
   ];
 
+  // for check box to check and dashed the task
   void checkBoxChanged(int index) {
     setState(() {
       toDOList[index][1] = !toDOList[index][1];
+    });
+  }
+
+  // for saving new task on the todo app
+  void saveNewTask() {
+    setState(() {
+      toDOList.add([_controller.text, false]);
+      _controller.clear();
+    });
+  }
+
+  // to delete the task from the todo app
+  void deleteTask(int index) {
+    setState(() {
+      toDOList.removeAt(index);
     });
   }
 
@@ -40,8 +57,48 @@ class _HomePageState extends State<HomePage> {
             taskName: toDOList[index][0],
             taskCompleted: toDOList[index][1],
             onChanged: (value) => checkBoxChanged(index),
+            deleteFunction: (context) => deleteTask(index),
           );
         },
+      ),
+      floatingActionButton: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                controller: _controller,
+                cursorColor: Colors.grey.shade500,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey.shade500),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey.shade500),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey.shade800,
+                  hintText: 'Add a new todo',
+                  hintStyle: TextStyle(
+                    color: Colors.grey.shade700,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          FloatingActionButton(
+            onPressed: saveNewTask,
+            backgroundColor: Colors.grey.shade500,
+            foregroundColor: Colors.grey.shade900,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(22),
+            ),
+            child: Icon(Icons.add),
+          ),
+        ],
       ),
     );
   }
--- lib/utils/todo_list.dart
+++ lib/utils/todo_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class TodoList extends StatefulWidget {
   const TodoList({
@@ -6,11 +7,13 @@ class TodoList extends StatefulWidget {
     required this.taskName,
     required this.taskCompleted,
     this.onChanged,
+    this.deleteFunction,
   });
 
   final String taskName;
   final bool taskCompleted;
   final Function(bool?)? onChanged;
+  final Function(BuildContext)? deleteFunction;
 
   @override
   State<TodoList> createState() => _TodoListState();
@@ -21,20 +24,48 @@ class _TodoListState extends State<TodoList> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 20, left: 20, right: 20, bottom: 0),
-      child: Container(
-        padding: const EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          color: Colors.grey.shade400,
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: Row(
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: StretchMotion(),
           children: [
-            Checkbox(value: widget.taskCompleted, onChanged: widget.onChanged),
-            Text(
-              widget.taskName,
-              style: TextStyle(color: Colors.grey.shade900, fontSize: 18),
+            SlidableAction(
+              onPressed: widget.deleteFunction,
+              icon: Icons.delete,
+              backgroundColor: Colors.grey.shade500,
+              borderRadius: BorderRadius.circular(25),
             ),
           ],
+        ),
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.grey.shade500,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Row(
+            children: [
+              Checkbox(
+                value: widget.taskCompleted,
+                onChanged: widget.onChanged,
+                checkColor: Colors.grey.shade300,
+                activeColor: Colors.grey.shade900,
+                side: BorderSide(color: Colors.grey.shade200),
+              ),
+              Text(
+                widget.taskName,
+                style: TextStyle(
+                  color: Colors.grey.shade900,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                  decoration:
+                      widget.taskCompleted
+                          ? TextDecoration.lineThrough
+                          : TextDecoration.none,
+                  decorationThickness: 5.5,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
